7ab7c8dfeabf7007f30cdcceadbdde66dd95fc3f
Brianna Bullock <brianna.bullock16@gmail.com>
2020-09-29 03:16:28 -0500

    Updates

---
+++ precache-manifest.f26ee89456f5a6f7f072d778a8300644.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bdd115342ccfc3935929a8833cf370f",
+    "url": "/bullock-portfolio-react/index.html"
+  },
+  {
+    "revision": "6f55b1ba89e824e3cd7a",
+    "url": "/bullock-portfolio-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cc82868cace3b4668196",
+    "url": "/bullock-portfolio-react/static/css/main.230550b8.chunk.css"
+  },
+  {
+    "revision": "6f55b1ba89e824e3cd7a",
+    "url": "/bullock-portfolio-react/static/js/2.c72ef2e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bullock-portfolio-react/static/js/2.c72ef2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc82868cace3b4668196",
+    "url": "/bullock-portfolio-react/static/js/main.3c4f5993.chunk.js"
+  },
+  {
+    "revision": "1979ed438ac7bf5619c2",
+    "url": "/bullock-portfolio-react/static/js/runtime-main.c3a454e5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.40f76e467d30ccd90303e08092564323.js"
+  "/bullock-portfolio-react/precache-manifest.f26ee89456f5a6f7f072d778a8300644.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bullock-portfolio-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
